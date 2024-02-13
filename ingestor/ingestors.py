4e95f0e0f82d0914a769ab4dc3a0ff3d426af0bb
Tian Luan <tiluan1999@hotmail.com>
2024-02-13 17:54:59 -0500

    Add csv ingestor

--- ingestor/ingestors.py
+++ ingestor/ingestors.py
@@ -4,3 +4,13 @@ class BaseIngestor:
 
     def __next__(self) -> str:
         return "not implemented"
+
+import pandas as pd   
+class CSVFileIngestor(BaseIngestor):
+    def __init__(self, file_path: str) -> None:
+        df = pd.read_csv(file_path)
+        self.iter = df.iterrows()
+        self.col_names = df.columns.to_list()
+    
+    def __next__(self) -> str:
+        return next(self.iter)
